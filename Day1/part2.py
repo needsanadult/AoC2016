43c2d12d63748c247a6b992647c19d76c3c1ed8b
uberfade <bradcernick@gmail.com>
2016-12-04 17:15:53 -0600

    Finished Day1 challenges

--- Day1/part2.py
+++ Day1/part2.py
@@ -1,8 +1,39 @@
+def pos_vert_coords(number):
+	for x in range(number):
+		alter_val = travels[len(travels)-1][1]
+		tupval = (travels[len(travels)-1][0], alter_val + 1)
+		travels.append(tuple(tupval))
+	return travels
+
+def neg_vert_coords(number):
+	for x in range(number):
+		alter_val = travels[len(travels)-1][1]
+		tupval = (travels[len(travels)-1][0], alter_val - 1)
+		travels.append(tuple(tupval))
+	return travels
+
+def pos_horz_coords(num):
+	for x in range(num):
+		alter_val = travels[len(travels)-1][0]
+		tupval = (alter_val + 1, travels[len(travels)-1][1])
+		travels.append(tuple(tupval))
+	return travels
+
+def neg_horz_coords(num):
+	for x in range(num):
+		alter_val = travels[len(travels)-1][0]
+		tupval = (alter_val - 1, travels[len(travels)-1][1])
+		travels.append(tuple(tupval))
+	return travels
+
+tst_dir = ['R2', 'L5', 'L1', 'L6']
+
 rawlist = 'R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1'
 
 dirs = rawlist.split(', ')
 
 loc = [0, 0]
+
 heading = 'N'
 
 def determine_heading(head, direction):
@@ -42,42 +73,58 @@ for x in dirs:
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		pos_horz_coords(dis)
 	elif x.startswith('L') and heading == 'N':
 		loc[0] = loc[0] - dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		neg_horz_coords(dis)
 	elif x.startswith('R') and heading == 'S':
 		loc[0] = loc[0] - dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		neg_horz_coords(dis)
 	elif x.startswith('L') and heading == 'S':
 		loc[0] = loc[0] + dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		pos_horz_coords(dis)
 	elif x.startswith('R') and heading == 'W':
 		loc[1] = loc[1] + dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		pos_vert_coords(dis)
 	elif x.startswith('L') and heading == 'W':
 		loc[1] = loc[1] - dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		neg_vert_coords(dis)
 	elif x.startswith('R') and heading == 'E':
 		loc[1] = loc[1] - dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		neg_vert_coords(dis)
 	else:
 		loc[1] = loc[1] + dis
 		print ('New Location:', loc)
 		heading = determine_heading(current_head, L_or_R)
 		print ('New Heading:', heading)
+		pos_vert_coords(dis)
+print (travels)
 
-dist = abs(loc[0]) + abs(loc[1])
 
-print (dist)
+vals = []
+for x in travels:
+  if travels.count(x) >=2:
+    vals.append(x)
+
+print (vals)
+
+blocks = vals[0][0] + vals[0][1]
+print (blocks)
